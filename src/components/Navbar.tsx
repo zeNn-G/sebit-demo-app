f2816fd207391f5c51ec47f456fd3fb535b4c520
zeNn-G <treroin@gmail.com>
2023-04-17 18:00:03 +0300

    Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,9 +4,10 @@ import { titleAtom } from "@/store/titleAtom";
 const Navbar = () => {
   const [title] = useAtom(titleAtom);
   return (
-    <div className="p-5">
-      <div>{title}</div>
-    </div>
+    <nav className="w-full">
+      <h1 className="text-white text-3xl font-bold uppercase">{title}</h1>
+      <div className="w-full h-1 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 mt-3 mb-5" />
+    </nav>
   );
 };
 
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "@/components";
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <>
-      <main className="h-full w-full xl:container mx-auto mt-8 px-10">
+      <main className="h-full w-full xl:w-[1200px] mx-auto mt-8 px-10">
         <Navbar />
         {children}
       </main>
