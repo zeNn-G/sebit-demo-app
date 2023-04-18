19c732b7f1cf718d97a23bf9d2cfb817143c5fa1
zeNn-G <treroin@gmail.com>
2023-04-18 16:20:15 +0300

    Css

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,8 +4,8 @@ import { titleAtom } from "@/store/titleAtom";
 const Navbar = () => {
   const [title] = useAtom(titleAtom);
   return (
-    <nav className="w-full pt-5">
-      <h1 className="text-white text-3xl font-bold uppercase">{title}</h1>
+    <nav className="w-full pt-5 ${exo_2.className}">
+      <h1 className="text-white text-3xl font-bold">{title}</h1>
       <div className="w-full h-1 bg-gradient-to-r from-pink-300 via-purple-300 to-indigo-400 mt-3 mb-5" />
     </nav>
   );
